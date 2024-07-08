6f8f0ddb208cae5d74cbd811791ecdf85ef1aaba
Nyx <max.ivan.97@mail.ru>
2024-07-08 16:31:16 +0300

    my change

--- TelegrammBot/obj/Debug/net8.0/TelegrammBot.AssemblyInfo.cs
+++ TelegrammBot/obj/Debug/net8.0/TelegrammBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegrammBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+703cf81c117134e19c37bfadcd5fc8bf5f617aca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegrammBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegrammBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
